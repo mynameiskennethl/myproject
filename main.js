b6eea328b28eb6925c1d8bc925a03b839ae2731b
mynameiskennethl <143271460+mynameiskennethl@users.noreply.github.com>
2023-08-27 22:42:30 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const clientId = "d0f4cbf4c19b4b93812d939de41e9627";
+const redirectUri = "http://127.0.0.1:5500/MINE/index.html";
+
+function authorizeUser() {
+    const scopes = 'user-read-private playlist-read-private';
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
+    window.location = authUrl;
+}
+
+async function getToken(authorizationCode) {
+    const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const tokenData = new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: authorizationCode,
+        redirect_uri: redirectUri,
+        client_id: clientId,
+    });
+    const tokenHeaders = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
+    const response = await fetch(tokenUrl, {
+        method: 'POST',
+        body: tokenData,
+        headers: tokenHeaders,
+    });
+    
+    const data = await response.json();
+    return data.access_token;
+}
+
+async function getUserPlaylists() {
+    const authorizationCode = new URLSearchParams(window.location.search).get('code');
+    if (!authorizationCode) {
+        authorizeUser();
+        return;
+    }
+
+    const accessToken = await getToken(authorizationCode);
+    
+    const playlistsUrl = 'https://api.spotify.com/v1/me/playlists';
+    const playlistsHeaders = {
+        Authorization: `Bearer ${accessToken}`,
+    };
+
+    const result = await fetch(playlistsUrl, {
+        headers: playlistsHeaders,
+    });
+
+    const data = await result.json();
+    console.log(data);
+}
+
+getUserPlaylists();
